fa46e9d5f2d3b8bbe20a81a1bd0f1bbb7e7865c2
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-12-09 19:26:50 -0300

    Agregar inicio de proyecto django y script sql para crear tablas

---
+++ backend/script.sql
@@ -0,0 +1,18 @@
+create database todolist;
+use disquería;
+
+create table usuarios (
+    id_usuario int not null auto_increment primary key,
+    user_name varchar(30) not null,
+    contraseña varchar(30) not null,
+    email varchar(30) not null,
+    nombre varchar(30) not null,
+    apellido varchar(30) not null,
+);
+
+create table notas (
+    id_nota int not null auto_increment primary key,
+    nombre varchar(30) not null,
+    completado boolean not null,
+    foreign key(id_usuario) references usuarios(id_usuario),
+);
