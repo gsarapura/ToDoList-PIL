06ed3dc639fec094cf78e798799238f9394ff9d7
Gustavs <gustavo.sarapura7@gmail.com>
2022-12-22 11:39:19 -0300

    Agregar formik a Login y cambiar ruta de imagens (/public)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,68 +13,57 @@ import {
   Spacer,
   InputGroup,
   InputRightElement} from "@chakra-ui/react";
+import { useFormik } from "formik";
 
 export const Login = () => {
+  // React router:
+  const navigate = useNavigate()
+
   // Show password:
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
 
-  // React router:
-  const navigate = useNavigate()
+  // Formik:
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password:""
+    },
+    onSubmit: (values) => {
+      axios
+        .post('http://localhost:8000/user/login/', values)
+        .then(response => {
+          console.log(response.data) //Context usuario tal. (Cookie)
+          navigate("/notas")
+        })
+        .catch(error => {
+          console.log(error.response.data)
+          alert("Hubo un error de ingreso.")
+        })
+    }
+  });
   
-  // Capturar info:
-  const [loginInfo, setLoginInfo] = useState({
-    username: "",
-    password: ""
-  })
-
-  function handleChange(e) {
-    setLoginInfo({
-      ...loginInfo,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("hola")
-    axios
-      .post('http://localhost:8000/user/login/', loginInfo)
-      .then(response => {
-        console.log(response.data) //Context usuario tal. (Cookie)
-        navigate("/notas")
-      })
-      .catch(error => {
-        console.log(error.response.data)
-        alert("Hubo un error de ingreso.")
-      })
-  } 
-  
-
   return(
     <Box as="main" borderColor="gray.200" p={6} boxShadow="2xl">
       <Heading as="h1" textAlign="center">Ingreso</Heading>
-
-      <FormControl onSubmit= { handleSubmit }>
-
+      <FormControl as="form" onSubmit= { formik.handleSubmit }>
         <FormLabel>Usuario</FormLabel>
         <Input 
           type="text" 
           name="username" 
           placeholder="Ingrese usuario"
-          onChange={ handleChange }
+          onChange={ formik.handleChange }
           mb={6}
         />
 
-
         <FormLabel >Contraseña</FormLabel>
-        <InputGroup size="md">
+        <InputGroup>
           <Input 
             className="form-control" 
             type={ show ? "text" : "password" } 
             name="password" 
             placeholder="Ingrese contraseña"
-            onChange={ handleChange }
+            onChange={ formik.handleChange }
             mb={6}
           />
           <InputRightElement width='4.5rem'>
