adfb9e897ebddb954f36b5be787623d6a6a34263
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-12-26 11:49:39 -0300

    Agregar chakra a componente BotonFiltrar

--- frontend/components/Notes/BotonFiltrar/BotonFiltrar.jsx
+++ frontend/components/Notes/BotonFiltrar/BotonFiltrar.jsx
@@ -1,13 +1,16 @@
+import { Button } from '@chakra-ui/react'
 export const BotonFiltrar = (props) => {
   return(
     <>
-      <button 
+      <Button
+        colorScheme="twitter"
         type="button" 
-        className="toggle-btn m-1"
+        m={1}
         aria-pressed={ props.isPressed }
+        className="toggle-btn"
         onClick={ () => props.setFilter(props.name) }>
         { props.name }
-      </button>
+      </Button>
     </>
   )
 }
